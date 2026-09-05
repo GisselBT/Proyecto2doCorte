70051d73b0124e931f45b640eda0de070874e4d0
Gissel BT <beltrantiqueelsygissel@gmail.com>
2026-09-05 15:35:34 -0500

    Creacion de 3 metodos constructores sobrecargados en la superClase y utilizando el this. para llamar a otro constructor de la misma clase

--- Proyecto2doCorte/src/proyecto2docorte/Trasporte.java
+++ Proyecto2doCorte/src/proyecto2docorte/Trasporte.java
@@ -8,5 +8,28 @@ public class Trasporte
   private int capacidad;
   private double precio;
   private boolean disponible;
+
+   //El this. funciona para llamar a un constructor de la misma clase 
+    public Trasporte() 
+    {
+        this.destino = "Sin destino";
+        this.capacidad = 0;
+        this.precio = 0.0;
+        this.disponible = false;
+    }
+
+    public Trasporte(String destino, double precio) 
+    {
+        this.destino = destino;
+        this.precio = precio;
+    }
+
+    public Trasporte(String destino, int capacidad, double precio, boolean disponible) 
+    {
+        this.destino = destino;
+        this.capacidad = capacidad;
+        this.precio = precio;
+        this.disponible = disponible;
+    } 
     
 }
