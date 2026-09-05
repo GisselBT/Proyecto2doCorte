6b759bceb9cc29eb4d206d1b896f0fa80f822946
Gissel BT <beltrantiqueelsygissel@gmail.com>
2026-09-05 18:27:16 -0500

    En este bloque de codigo agregamos los constructores en la clase hija Tren y el super() para llamar constructores de la super clase

--- Proyecto2doCorte/src/proyecto2docorte/Tren.java
+++ Proyecto2doCorte/src/proyecto2docorte/Tren.java
@@ -14,5 +14,32 @@ public class Tren
     private int numeroVagones;
     private double velocidadMaxima;
     private boolean danComida;
+
+    //CONSTRUCTORES
+
+    public Tren() 
+    {
+        super();
+        this.tipoTren = "Sin tren";
+        this.numeroVagones = 0;
+        this.velocidadMaxima = 0.0;
+        this.danComida = false;
+    }
+
+    public Tren(String tipoTren, int numeroVagones, String destino, double precio) 
+    {
+        super(destino, precio);
+        this.tipoTren = tipoTren;
+        this.numeroVagones = numeroVagones;
+    }
+
+    public Tren(String tipoTren, int numeroVagones, double velocidadMaxima, boolean danComida, String destino, int capacidad, double precio, boolean disponible) 
+    {
+        super(destino, capacidad, precio, disponible);
+        this.tipoTren = tipoTren;
+        this.numeroVagones = numeroVagones;
+        this.velocidadMaxima = velocidadMaxima;
+        this.danComida = danComida;
+    } 
     
 }
