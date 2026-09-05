e725494ceddbd8340a33c951f7c7f981c8f7e2fd
Gissel BT <beltrantiqueelsygissel@gmail.com>
2026-09-05 18:16:03 -0500

    En este bloque de codigo agregamos los constructores y el super() para llamar constructores de la super clase

--- Proyecto2doCorte/src/proyecto2docorte/Avion.java
+++ Proyecto2doCorte/src/proyecto2docorte/Avion.java
@@ -14,5 +14,33 @@ public class Avion
     private int numeroVuelo;
     private double horasVuelo;
     private boolean escala;
-    
+
+    public Avion() 
+    {
+        /*A qui el super(); llama al constructor vacio de Transporte
+        y despues inicializa los 4 atributos de Avion*/
+        super ();
+        this.aerolinea = "Sin aerolinea";
+        this.numeroVuelo = 0;
+        this.horasVuelo = 0.0;
+        this.escala = false;
+    }
+
+    public Avion(String aerolinea, int numeroVuelo, String destino, double precio) 
+    {
+        //Aca llama a este constructor de Transporte 
+        super(destino, precio);
+        this.aerolinea = aerolinea;
+        this.numeroVuelo = numeroVuelo;
+    }
+
+    public Avion(String aerolinea, int numeroVuelo, double horasVuelo, boolean escala, String destino, int capacidad, double precio, boolean disponible) 
+    {
+        //Aca llama el constructor completo de la super clase
+        super(destino, capacidad, precio, disponible);
+        this.aerolinea = aerolinea;
+        this.numeroVuelo = numeroVuelo;
+        this.horasVuelo = horasVuelo;
+        this.escala = escala;
+    }   
 }
